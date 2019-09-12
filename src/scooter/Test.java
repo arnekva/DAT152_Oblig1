6129eecc77f0379fce0328102c7e04a8f2d6580f
Arne Kvaleberg <arnekva@gmail.com>
2019-09-12 12:14:17 +0200

    Fixed call command

--- src/scooter/Test.java
+++ src/scooter/Test.java
@@ -15,55 +15,54 @@ public class Test {
 		Scanner tast = new Scanner(System.in);
 		String help = "call: See alle varer \nadd:Skriv add, trykk enter, så følg opp med varenummeret og trykk enter."
 				+ "\ncart: Se carten din";
-		
+
 		System.out.println("Velkommen til The Big Cup Store! Skriv 'help' for en liste over kommandoer:");
 		boolean finished = false;
-		while(!finished) {
-			
-		String input = tast.nextLine();
-		
-		switch (input) {
-		case "call": 
-			for(int i = 0; i<products.size();i++) {
-				System.out.println(products.get(i).toString());
+		while (!finished) {
+
+			String input = tast.nextLine();
+
+			switch (input) {
+			case "call":
+				for (int i = 0; i < products.size(); i++) {
+					System.out.println(products.get(i).toString());
+				}
+				break;
+			case "add":
+				try {
+					int nr = tast.nextInt();
+					tast.nextLine();
+					for (int j = 0; j < products.size(); j++) {
+						if (products.get(j).getProdnr() == nr) {
+							cart.addItem(products.get(j));
+							System.out.println("Vare lagt til");
+						}
+					}
+
+				} catch (Exception e) {
+					System.out.println("fuck you");
+				}
+				break;
+			case "cart":
+				System.out.println("Du har følgende varer i handlekurven: ");
+				for (int i = 0; i < cart.getItems().size(); i++) {
+					System.out.println(cart.getItems().get(i));
+				}
+				break;
+			case "help":
+				System.out.println(help);
+				break;
+			case "done":
+				finished = true;
+				System.out.println("Takk for besøket, velkommen igjen :-)");
+				break;
+			default:
+				System.out.println("Denne kommandoen gjenkjente vi ikke. Prøv på ny. Skriv 'help' for listen igjen.");
+				break;
 			}
-		case "add":
-			try {
-			int nr = tast.nextInt();tast.nextLine();
-			for(int j = 0; j<products.size();j++) {
-				if(products.get(j).getProdnr() == nr) {
-					cart.addItem(products.get(j));
-					System.out.println("Vare lagt til");
-				} 
-			}
-			
-			} catch(Exception e) {
-				System.out.println("fuck you");
-			}
-			break;
-		case "cart":
-			System.out.println("Du har følgende varer i handlekurven: ");
-			for(int i = 0; i<cart.getItems().size(); i++) {
-				System.out.println(cart.getItems().get(i));
-			}
-			break;
-		case "help":
-			System.out.println(help);
-			break;
-		case "done":
-			finished = true;
-			break;
-		default:
-			System.out.println("Denne kommandoen gjenkjente vi ikke. Prøv på ny. Skriv 'help' for listen igjen.");
-			break;
+
 		}
-		
-		
-		
-		}
-		
-		
-		
+
 	}
 
 }

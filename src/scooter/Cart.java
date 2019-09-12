8a01cf86dce88fe6f3b47cb3e9a27ccc4fe0faee
571734 <571734@stud.hvl.no>
2019-09-12 17:43:14 +0200

    slettet Cart

--- src/scooter/Cart.java
+++ src/scooter/Cart.java
@@ -1,37 +1,25 @@
 package scooter;
 
 import java.util.ArrayList;
-import java.util.List;
 
-//import javax.persistence.EntityManagerFactory;
+
 
 public class Cart {
 	
+	ArrayList<Product> CartItems;
 	
-	private List<Product> items = new ArrayList<>();
+	public Cart() {
+		this.CartItems = new ArrayList<Product>();
+	}
 	
-	public void addItem(Product item) {
-		items.add(item);
+	public ArrayList<Product> getAllCartItems() {
+		return CartItems;
 	}
-
-	public void removeAllInstancesOfItem(Product item) {
-
-//		for (int i = 0; i < items.size(); i++) {
-//			if (items.get(i).getName().equals(item.getName())) {
-//				items.remove(i);
-//			}
-//		}
+	public void addItem(Product product) {
+		CartItems.add(product);
 	}
-	public void remove(Product item){
-		items.remove(item);
+	public void removeItem(Product product) {
+		CartItems.remove(product);
 	}
-
-	public void removeAll() {
-		items.clear();
-	}
-
-	public List<Product> getItems() {
-		return items;
-	}
-
+	
 }

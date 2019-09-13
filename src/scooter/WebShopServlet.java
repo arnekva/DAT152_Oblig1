69843eac8dc4685bc38ad6960b65128968e8845c
571734 <571734@stud.hvl.no>
2019-09-13 13:39:57 +0200

    fikset are sin bug

--- src/scooter/WebShopServlet.java
+++ src/scooter/WebShopServlet.java
@@ -1,6 +1,5 @@
 package scooter;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,45 +19,47 @@ public class WebShopServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	ProductEAO productEAO = new ProductEAO();
 	Cart cart;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public WebShopServlet() {
-        super();
-        
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public WebShopServlet() {
+		super();
+
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession sesjon = null;
 		if (request.getSession(false) == null) {
 			sesjon = request.getSession(true);
 		} else {
 			sesjon = request.getSession(false);
 		}
-		
-		
+
 		if (request.getSession().getAttribute("cart") == null) {
 			cart = new Cart();
 		} else {
 			cart = (Cart) request.getSession().getAttribute("cart");
 		}
-		
+
 		ArrayList<Product> products = productEAO.getProducts();
-		
+
 		request.getSession().setAttribute("products", products);
 		request.getSession().setAttribute("cart", cart);
-		
-		if(request.getSession().getAttribute("language") == null || request.getSession().getAttribute("language") == "") {
+
+		if (request.getSession().getAttribute("language") == null
+				|| request.getSession().getAttribute("language") == "") {
 			request.getSession().setAttribute("language", "nb_NO");
 		} else {
 			System.out.println("Locale has been set by user");
 		}
-		
+
 		String locale = (String) request.getSession().getAttribute("language");
 		String langCode = "";
 		if (locale.equals("nb_NO")) {
@@ -68,7 +69,9 @@ public class WebShopServlet extends HttpServlet {
 		} else {
 			langCode = "DE";
 		}
-		
+		System.out.println(locale);
+		System.out.println(langCode);
+
 		DescriptionEAO descriptionEAO = new DescriptionEAO();
 		ArrayList<Description> descriptions = new ArrayList<Description>();
 		for (Description d : descriptionEAO.getDescriptions()) {
@@ -77,31 +80,38 @@ public class WebShopServlet extends HttpServlet {
 			}
 		}
 		Collections.sort(descriptions);
-		
+
 		request.getSession().setAttribute("descriptions", descriptions);
 
 		request.getRequestDispatcher("WEB-INF/products.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		cart = (Cart) request.getSession().getAttribute("cart");
 		Product cartItem;
-		
-		try {
-			
-			cartItem = productEAO.getProduct(Integer.parseInt(request.getParameter("prodnr")));
-			System.out.println(cartItem);
-			cart.addItem(cartItem);
-		} catch (NullPointerException e) {
-			System.out.println("Hmm.. dette ser ikke ut som et gyldig produkt.");
+		if (request.getParameter("language") != null) {
+			String locale = (String) request.getParameter("language");
+			request.getSession().setAttribute("language", locale);
+		}
+		if (request.getParameter("prodnr") != null) {
+			try {
+
+				cartItem = productEAO.getProduct(Integer.parseInt(request.getParameter("prodnr")));
+				System.out.println(cartItem);
+				cart.addItem(cartItem);
+			} catch (Exception e) {
+				System.out.println("Hmm.. dette ser ikke ut som et gyldig produkt.");
+			}
 		}
 		request.getSession().setAttribute("cart", cart);
 		response.sendRedirect("products");
-		
+
 	}
 
 }

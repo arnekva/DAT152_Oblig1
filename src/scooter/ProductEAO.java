2bb5e80a40d0e94389eddfb71e7becc9d081193d
571734 <571734@stud.hvl.no>
2019-09-13 12:19:54 +0200

    Fikset beskrivelser

--- src/scooter/ProductEAO.java
+++ src/scooter/ProductEAO.java
@@ -10,12 +10,12 @@ public class ProductEAO {
 	
 	
 	public ProductEAO() {
-		Product kopp1 = new Product(1, "HaraldCup", 9, "img/kopp1.png");
+		Product kopp1 = new Product(1, "MunicipalCup", 9, "img/kopp1.png");
 		Product kopp2 = new Product(2, "The Golden Mug", 29, "img/kopp2.png");
 		Product kopp3 = new Product(3, "Goblet of Fire", 199, "img/kopp3.png");
 		Product kopp4 = new Product(4, "The 99 Coffee cup", 12, "img/kopp4.png");
 		Product kopp5 = new Product(5, "Are's Favorite", 0.99, "img/kopp5.png");
-		Product kopp6 = new Product(6, "Big D Cup", 499, "img/kopp6.png");
+		Product kopp6 = new Product(6, "The Big D Cup", 499, "img/kopp6.png");
 		
 		products.add(kopp1);
 		products.add(kopp2);

70f22bf1f576f44b19eb84ef06dd7bbc68dd66f0
571734 <571734@stud.hvl.no>
2019-09-13 11:36:52 +0200

    Masse Description greier

--- src/scooter/Description.java
+++ src/scooter/Description.java
@@ -1,5 +1,40 @@
 package scooter;
 
 public class Description {
+	
+	private int prodnr;
+	private String langCode;
+	private String txt;
+	
+	public Description(int prodnr, String langCode, String txt) {
+		this.prodnr = prodnr;
+		this.langCode = langCode;
+		this.txt = txt;
+	}
 
+	public int getProdnr() {
+		return prodnr;
+	}
+
+	public void setProdnr(int prodnr) {
+		this.prodnr = prodnr;
+	}
+
+	public String getLangCode() {
+		return langCode;
+	}
+
+	public void setLangCode(String langCode) {
+		this.langCode = langCode;
+	}
+
+	public String getTxt() {
+		return txt;
+	}
+
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	
+	
 }
---
+++ src/scooter/DescriptionEAO.java
@@ -0,0 +1,53 @@
+package scooter;
+
+import java.util.ArrayList;
+
+public class DescriptionEAO {
+	
+	ArrayList<Description> descriptions;
+	
+	
+	public DescriptionEAO() {
+		descriptions = new ArrayList<Description>();
+		
+		Description desc1 = new Description(1, "NB", "Haraldkoppen - Vår mest solgte kopp. Denne koppen sørger for at du alltid har et sted å gjøre av væsken din.");
+		Description desc2 = new Description(2, "NB", "Gullkoppen - Gjør stort sett det samme som Haraldkoppen, men den sporter en fresh gullfarge.");
+		Description desc3 = new Description(3, "NB", "Ildbegeret - Du skulle kanskje tro at denne er hentet rett fra Harry Potter, men det hadde ikke vært en klok tanke.. Koppen som bokstavelig talt står i flammer! NB:brannfare.");
+		Description desc4 = new Description(4, "NB", "99koppen - Som barn lekte du gjerne politi og røver. Som voksen kan du nå ta leken til et helt nytt nivå men denne koppen. Len deg tilbake i godstolen og ta deg en ordentlig kopp kaffe i denne politikoppen!");
+		Description desc5 = new Description(5, "NB", "");
+		Description desc6 = new Description(6, "NB", "");
+		Description desc7 = new Description(1, "EN", "");
+		Description desc8 = new Description(2, "EN", "");
+		Description desc9 = new Description(3, "EN", "");
+		Description desc10 = new Description(4, "EN", "");
+		Description desc11 = new Description(5, "EN", "");
+		Description desc12 = new Description(6, "EN", "");
+		Description desc13 = new Description(1, "DE", "");
+		Description desc14 = new Description(2, "DE", "");
+		Description desc15 = new Description(3, "DE", "");
+		Description desc16 = new Description(4, "DE", "");
+		Description desc17 = new Description(5, "DE", "");
+		Description desc18 = new Description(6, "DE", "");
+		
+		descriptions.add(desc1);
+		descriptions.add(desc2);
+		descriptions.add(desc3);
+		descriptions.add(desc4);
+		descriptions.add(desc5);
+		descriptions.add(desc6);
+		descriptions.add(desc7);
+		descriptions.add(desc8);
+		descriptions.add(desc9);
+		descriptions.add(desc10);
+		descriptions.add(desc11);
+		descriptions.add(desc12);
+		descriptions.add(desc13);
+		descriptions.add(desc14);
+		descriptions.add(desc15);
+		descriptions.add(desc16);
+		descriptions.add(desc17);
+		descriptions.add(desc18);
+		
+		
+	}
+}

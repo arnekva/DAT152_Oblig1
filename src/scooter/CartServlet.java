69a5eaa1758aca6acfe62bfb717c733dfc4f0712
571734 <571734@stud.hvl.no>
2019-09-13 17:23:30 +0200

    styrer med noen greier..

--- src/scooter/CartServlet.java
+++ src/scooter/CartServlet.java
@@ -2,6 +2,7 @@ package scooter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -31,6 +32,45 @@ public class CartServlet extends HttpServlet {
 		} else {
 			System.out.println("Locale has been set by user");
 		}
+		
+		String locale = (String) request.getSession().getAttribute("language");
+		String langCode = "";
+		double currencyMultiplier = 1;
+		if (locale.equals("nb_NO")) {
+			langCode = "NB";
+			currencyMultiplier = 9.936;
+		} else if (locale.equals("en_US")) {
+			langCode = "EN";
+			currencyMultiplier = 1.108;
+		} else {
+			langCode = "DE";
+		}
+		System.out.println(locale);
+		System.out.println(langCode);
+
+		DescriptionEAO descriptionEAO = new DescriptionEAO();
+		ArrayList<Description> allDescriptions = descriptionEAO.getDescriptions();
+		Cart cart = (Cart) request.getSession().getAttribute("cart");
+		ArrayList<Product> cartItems = cart.getItems();
+		ArrayList<Description> descriptions = new ArrayList<Description>();
+		for (Product p : cartItems) {
+			for (int i = 0; i < allDescriptions.size(); i++) {
+				if (p.getProdnr() == allDescriptions.get(i).getProdnr() && allDescriptions.get(i).getLangCode().equals(langCode)) {
+					descriptions.add(allDescriptions.get(i));
+					i = allDescriptions.size();
+				}
+			}
+			
+		}
+		productEAO = new ProductEAO();
+		ArrayList<Product> productList = productEAO.getProducts();
+		ArrayList<Product> rightPriceTiles = cartItems;
+		for (Product p : rightPriceTiles) {
+			p.setPris(p.getPris()*currencyMultiplier);
+		}
+		request.getSession().setAttribute("products", rightPriceTiles);
+		request.getSession().setAttribute("descriptions", descriptions);
+		
 		request.getRequestDispatcher("WEB-INF/cart.jsp").forward(request, response);
 	}
 
